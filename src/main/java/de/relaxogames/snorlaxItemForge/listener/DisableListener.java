3ed5ddeea7f73801d603d8063f570500fa14d082
Johannes (jotrorox) Müller <mail@jotrorox.com>
2025-12-25 23:52:35 +0100

    Fixing the DisableListener

--- src/main/java/de/relaxogames/snorlaxItemForge/listener/DisableListener.java
+++ src/main/java/de/relaxogames/snorlaxItemForge/listener/DisableListener.java
@@ -1,75 +1,43 @@
 package de.relaxogames.snorlaxItemForge.listener;
 
-import de.relaxogames.snorlaxItemForge.FileManager;
 import org.bukkit.Particle;
-import org.bukkit.damage.DamageSource;
+import org.bukkit.World;
 import org.bukkit.entity.EnderCrystal;
 import org.bukkit.entity.Entity;
-import org.bukkit.entity.TNTPrimed;
 import org.bukkit.entity.minecart.ExplosiveMinecart;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
-import org.bukkit.event.block.TNTPrimeEvent;
 import org.bukkit.event.entity.EntityDamageByEntityEvent;
-import org.bukkit.event.entity.EntityDamageEvent;
 import org.bukkit.event.entity.EntityExplodeEvent;
 
 public class DisableListener implements Listener {
 
-    private FileManager fileManager = new FileManager();
-
     @EventHandler
-    public void onCrystal(EntityExplodeEvent e){
-        if (!fileManager.disabledEndCrystals())return;
-        Entity exploder = e.getEntity();
-        if (!(exploder instanceof EnderCrystal crystal))return;
-        e.setCancelled(true);
-        crystal.remove();
-        exploder.getLocation().getWorld().spawnParticle(Particle.HEART, e.getLocation(), 20, 3, 3, 3);
-    }
-
-    @EventHandler
-    public void onTNTExplode(TNTPrimeEvent e){
-        if (!fileManager.disabledTNT())return;
-        e.setCancelled(true);
-    }
-
-    @EventHandler
-    public void onEntityExplode(EntityDamageByEntityEvent e){
-        Entity entity = e.getEntity();
-
-        // TNT
-        if (entity instanceof TNTPrimed && fileManager.disabledTNT()) {
-            e.setCancelled(true);
-            entity.remove();
-            entity.getWorld().spawnParticle(Particle.HEART, entity.getLocation(), 20, 3,3,3);
+    public void onCrystalExplode(EntityExplodeEvent e) {
+        if (!(e.getEntity() instanceof EnderCrystal crystal))
             return;
-        }
 
-        // TNT-Minecart
-        if (entity instanceof ExplosiveMinecart && fileManager.disabledTNTMinecart()) {
+        if (e.getLocation().getWorld().getEnvironment() != World.Environment.THE_END) {
             e.setCancelled(true);
-            entity.remove();
-            entity.getWorld().spawnParticle(Particle.HEART, entity.getLocation(), 20, 3,3,3);
-            return;
-        }
-
-        // Ender Crystal
-        if (entity instanceof EnderCrystal && fileManager.disabledEndCrystals()) {
-            e.setCancelled(true);
-            entity.remove();
-            entity.getWorld().spawnParticle(Particle.HEART, entity.getLocation(), 20, 3,3,3);
+            crystal.remove();
+            e.getLocation().getWorld().spawnParticle(Particle.HEART, e.getLocation(), 20, 3, 3, 3);
+        } else {
+            e.blockList().clear();
         }
     }
 
     @EventHandler
-    public void onTNTMinecart(EntityExplodeEvent e){
-        if (!fileManager.disabledTNTMinecart())return;
-        Entity exploder = e.getEntity();
-        if (!(exploder instanceof ExplosiveMinecart tntMinecart))return;
-        e.setCancelled(true);
-        tntMinecart.remove();
-        exploder.getLocation().getWorld().spawnParticle(Particle.HEART, e.getLocation(), 20, 3, 3, 3);
+    public void onTNTMinecartExplode(EntityExplodeEvent e) {
+        if (!(e.getEntity() instanceof ExplosiveMinecart))
+            return;
+        e.blockList().clear();
     }
 
+    @EventHandler
+    public void onDamageByEntity(EntityDamageByEntityEvent e) {
+        Entity damager = e.getDamager();
+        if (damager instanceof EnderCrystal || damager instanceof ExplosiveMinecart) {
+            e.setCancelled(true);
+        }
+    }
 }
